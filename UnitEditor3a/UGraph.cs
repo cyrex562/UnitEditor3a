cb4a9d4a688243731a68925f6969c359cd539eaf
Josh Madden <jomadde@microsoft.com>
2018-04-30 09:56:09 -0400

    Graph methods implemented; checking and testing load graph from file

--- UnitEditor3a/UGraph.cs
+++ UnitEditor3a/UGraph.cs
@@ -12,55 +12,67 @@ namespace UnitEditor3a
         [DataMember]
         public Guid GraphId {get;set;}
         [DataMember]
-        private Dictionary<Guid, UVertex> vertices;
+        public Dictionary<Guid, UVertex> Vertices { get; }
         [DataMember]
-        private Dictionary<Guid, UEdge> edges;
-        
-        // adjacent matrix: a 2-D matrix in which the rows represent source vertices and colums represent dest vertices
-        //private bool[,] _adjacencyMatrix;
-        // TODO: implement adjacency matrix
+        public Dictionary<Guid, UEdge> Edges { get; }
+
+        // adjacent matrix: a 2-D matrix in which the rows represent source vertices and columns represent dest vertices
+        [DataMember]
+        public Dictionary<Guid, List<Guid>> AdjacencyMatrix { get; }
 
         // incidence matrix: a 2-D boolean matrix in which the rows represent vertices and columns represent edges.
-        //private bool[,] _incidenceMatrix;
-        // TODO: implement incidence matrix
+        // List of edges for a given Vertex ID
+        [DataMember]
+        public Dictionary<Guid, List<Guid>> IncidenceMatrix { get; }
 
         public UGraph()
         {
-            this.vertices = new Dictionary<Guid, UVertex>();
-            this.edges = new Dictionary<Guid, UEdge>();
+            this.Vertices = new Dictionary<Guid, UVertex>();
+            this.Edges = new Dictionary<Guid, UEdge>();
             this.GraphId = new Guid();
-            //this._adjacencyMatrix = new bool[Defines.INIT_NUM_ADJ_MAT_ELE, 
-            //    Defines.INIT_NUM_ADJ_MAT_ELE];
-            //this._incidenceMatrix = new bool[Defines.INIT_NUM_INC_MAT_ELE, 
-            //    Defines.INIT_NUM_INC_MAT_ELE];
+            this.AdjacencyMatrix = new Dictionary<Guid, List<Guid>>();
+            this.IncidenceMatrix = new Dictionary<Guid, List<Guid>>();
         }
 
-        public int VertexCount => vertices.Count;
+        public int VertexCount => Vertices.Count;
 
-        public int EdgeCount => edges.Count;
-
-        public bool Adjacent(UVertex x, UVertex y)
-        {
-            return false;
-        }
-
-        public Dictionary<Guid, UVertex> Vertices => this.vertices;
-
-        public Dictionary<Guid, UEdge> Edges => this.edges;
+        public int EdgeCount => Edges.Count;
 
         // adjacent: whether there is an edge from vertex x to vertex y
-        public List<Guid> Neighbors(Guid vertexGuid)
+        public bool Adjacent(Guid headVertexId, Guid tailVertexId)
         {
-            List<Guid> outList = new List<Guid>();
-            return outList;
+            if (this.Vertices.ContainsKey(headVertexId) == false)
+            {
+                return false;
+            }
+
+            UVertex uv = this.Vertices[headVertexId];
+          
+            foreach (Guid neighId in uv.Neighbors)
+            {
+                if (neighId == tailVertexId)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        // neighbors: list all neighbors of a given vertex.
+        public List<Guid> Neighbors(Guid vertexId)
+        {
+            UVertex uv = this.Vertices[vertexId];
+            return uv.Neighbors;
         }
 
+        // check for the existence of a vertex with a given ID
         public bool VertexExists(Guid nodeId)
         {
-            return this.vertices.ContainsKey(nodeId);
+            return this.Vertices.ContainsKey(nodeId);
         }
 
-        
+        // Add a vertex
         public bool AddVertex(UVertex vertexToAdd)
         {
             if (VertexExists(vertexToAdd.VertexId) == true)
@@ -68,44 +80,98 @@ namespace UnitEditor3a
                 return false;
             }
 
-            this.vertices.Add(vertexToAdd.VertexId, vertexToAdd);
+            this.Vertices.Add(vertexToAdd.VertexId, vertexToAdd);
+            this.AdjacencyMatrix.Add(vertexToAdd.VertexId, new List<Guid>());
+            this.IncidenceMatrix.Add(vertexToAdd.VertexId, new List<Guid>());
             return true;
         }
 
-
-        public bool RemoveVertex(UVertex vertexToRemove)
+        // Remove a vertex
+        public bool RemoveVertex(Guid vertexToRemove)
         {
-            return false;
+            if (!VertexExists(vertexToRemove))
+            {
+                return false;
+            }
+            this.Vertices.Remove(vertexToRemove);
+            this.AdjacencyMatrix.Remove(vertexToRemove);
+            return true;
         }
 
-        public bool AddEdge(UVertex x, UVertex y)
+        // Add an edge
+        public bool AddEdge(UEdge edgeToAdd)
         {
-            return false;
+            if (this.Edges.ContainsKey(edgeToAdd.EdgeId))
+            {
+                return false;
+            }
+            this.Edges.Add(edgeToAdd.EdgeId, edgeToAdd);
+            this.AdjacencyMatrix[edgeToAdd.HeadVertexId].Add(edgeToAdd.TailVertexId);
+            this.AdjacencyMatrix[edgeToAdd.TailVertexId].Add(edgeToAdd.HeadVertexId);
+            this.IncidenceMatrix[edgeToAdd.HeadVertexId].Add(edgeToAdd.EdgeId);
+            this.IncidenceMatrix[edgeToAdd.TailVertexId].Add(edgeToAdd.EdgeId);
+            return true;
         }
 
-        public bool RemoveEdge(UVertex x, UVertex y)
+        // Remove an edge
+        public bool RemoveEdge(Guid edgeToRemove)
         {
-            return false;
+            UEdge ue = this.Edges[edgeToRemove];
+            Guid headVertexId = ue.HeadVertexId;
+            Guid tailVertexId = ue.TailVertexId;
+            if (!this.Edges.ContainsKey(edgeToRemove))
+            {
+                return false;
+            }
+            this.Edges.Remove(edgeToRemove);
+            this.AdjacencyMatrix[headVertexId].Remove(tailVertexId);
+            this.AdjacencyMatrix[tailVertexId].Remove(headVertexId);
+            this.IncidenceMatrix[headVertexId].Remove(edgeToRemove);
+            this.IncidenceMatrix[tailVertexId].Remove(edgeToRemove);
+            return true;
         }
 
-        public int GetVertexValue(UVertex x)
+        // Get a vertexes' value
+        public int GetVertexValue(Guid vertexId)
         {
-            return -1;
+            if (!this.VertexExists(vertexId))
+            {
+                return -1;
+            }
+            return this.Vertices[vertexId].Value;
         }
 
-        public bool SetVertexValue(UVertex x, int newValue)
+        // Set a vertexe's value
+        public bool SetVertexValue(Guid vertexId, int newValue)
         {
-            return false;
+            if (!this.VertexExists(vertexId))
+            {
+                return false;
+            }
+            UVertex uv = this.Vertices[vertexId];
+            uv.Value = newValue;
+            return true;
         }
 
-        public int GetEdgeValue(UVertex x, UVertex y)
+        // Get an edge's value
+        public int GetEdgeValue(Guid edgeId)
         {
-            return -1;
+            if (!this.Edges.ContainsKey(edgeId))
+            {
+                return -1;
+            }
+            return this.Edges[edgeId].Value;
         }
 
-        public bool SetEdgeValue(UVertex x, UVertex y)
+        // Set an edge's value
+        public bool SetEdgeValue(Guid edgeId, int newValue)
         {
-            return false;
+            if (!this.Edges.ContainsKey(edgeId))
+            {
+                return false;
+            }
+            this.Edges[edgeId].Value = newValue;
+            return true;
         }
     }
 }
